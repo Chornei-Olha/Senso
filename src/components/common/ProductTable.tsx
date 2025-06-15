1dd4ccec83cc003d36899d5fce5e9e83f573ea9a
workProject085 <workProject085@gmail.com>
2025-06-15 04:10:47 +0300

    first commit

--- src/components/common/ProductTable.tsx
+++ src/components/common/ProductTable.tsx
@@ -14,7 +14,7 @@ type ProductTableProps = {
 const ProductTable: React.FC<ProductTableProps> = ({ rows }) => {
   return (
     <div className="overflow-x-auto mt-6">
-      <table className="min-w-full table-auto border border-gray-300 text-sm md:text-base">
+      <table className="min-w-full table-auto border border-gray-300 text-xs md:text-base">
         <thead className="bg-gray-100">
           <tr>
             <th className="border border-white px-4 py-2 text-left">Codice</th>
