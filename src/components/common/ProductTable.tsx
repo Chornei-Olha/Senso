459c7cdad7d249d7f735ef8dcb986ad529932212
workProject085 <workProject085@gmail.com>
2025-06-15 04:13:01 +0300

    first commit

--- src/components/common/ProductTable.tsx
+++ src/components/common/ProductTable.tsx
@@ -17,19 +17,19 @@ const ProductTable: React.FC<ProductTableProps> = ({ rows }) => {
       <table className="min-w-full table-auto border border-gray-300 text-xs md:text-base">
         <thead className="bg-gray-100">
           <tr>
-            <th className="border border-white px-4 py-2 text-left">Codice</th>
-            <th className="border border-white px-4 py-2 text-left">Nome prodotto</th>
-            <th className="border border-white px-4 py-2 text-left">Colore</th>
-            <th className="border border-white px-4 py-2 text-left">Dimensione</th>
+            <th className="border border-white px-2 py-2 text-left">Codice</th>
+            <th className="border border-white px-2 py-2 text-left">Nome prodotto</th>
+            <th className="border border-white px-2 py-2 text-left">Colore</th>
+            <th className="border border-white px-2 py-2 text-left">Dimensione</th>
           </tr>
         </thead>
         <tbody>
           {rows.map((row, i) => (
             <tr key={i}>
-              <td className="border border-white px-4 py-2">{row.codice}</td>
-              <td className="border border-white px-4 py-2">{row.nome}</td>
-              <td className="border border-white px-4 py-2">{row.colore}</td>
-              <td className="border border-white px-4 py-2">{row.dimensione}</td>
+              <td className="border border-white px-2 py-2">{row.codice}</td>
+              <td className="border border-white px-2 py-2">{row.nome}</td>
+              <td className="border border-white px-2 py-2">{row.colore}</td>
+              <td className="border border-white px-2 py-2">{row.dimensione}</td>
             </tr>
           ))}
         </tbody>
